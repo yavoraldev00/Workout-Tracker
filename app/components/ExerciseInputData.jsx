b0fb1d3c83fde6dd7744341845796fee292b6f45
YAVOR ALDEV <y.aldev@student.fontys.nl>
2025-09-14 20:13:56 +0200

    Added table for tracking exercise load

---
+++ app/components/ExerciseInputData.jsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useEffect } from "react";
+import { IoAdd, IoRemove } from "react-icons/io5";
+
+
+export default function ExerciseInputData({ numberOfSets, setNumberOfSets }) {
+  useEffect(()=>{},[numberOfSets])
+  
+  return (
+    <div>
+        {
+            Array.from({ length: numberOfSets }, (_, i) => i + 1)
+            .map((set_number) => (
+                <>
+                    {
+                        (set_number == 1) ? 
+                        <div></div>
+                        :
+                        <button onClick={(e)=>{e.preventDefault; setNumberOfSets(numberOfSets - 1)}} className="mx-auto"><IoRemove /></button>
+                   
+                   }
+
+
+                    <input type="number" name="reps" className="bg-amber-200"/>
+                    <input type="number" name="weight" className="bg-amber-200"/>
+                </>
+            ))
+
+            
+        }
+
+        {numberOfSets < 6 && (
+        <button onClick={(e)=>{e.preventDefault; setNumberOfSets(numberOfSets + 1)}} className="mx-auto"><IoAdd /></button>
+      )}
+    </div>
+  )
+}
--- app/components/ExerciseSpecifics.jsx
+++ app/components/ExerciseSpecifics.jsx
@@ -2,30 +2,28 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { IoAdd } from "react-icons/io5";
+import ExerciseInputData from "./ExerciseInputData";
 
 export default function ExerciseSpecifics() {
-  const [sets, setSets] = useState(1)
+  const [sets, setSets] = useState(3)
+  const [setLoad, setSetLoad] = useState([[]])
 
     return (
     <div className="flex">
-        <Image src="/exercise_img/2Qh2J1e.gif" width={128} height={128}/>
+        <Image src="/exercise_img/2Qh2J1e.gif" alt="shhh" width={128} height={128}/>
         <div>
             <h3>exercise name</h3>
 
-            {/* Number of reps and weight */}
-            <div className="flex">
-                {/* Shows button if less than 5 sets */}
-                <button>
-                    <IoAdd />
-                </button>
+            <div className="exercise-load-grid">
+                <div><span>Set</span><span>Reps</span><span>Weight</span></div>
+                    {/* Number of reps and weight */}
+                
+                {/* This is gonna be the component */}
 
-                {/* Shows if the add button has been clicked, so add button doesn't appear with these */}
-                <input name="sets"/>
-                <input name="sets"/>
+                <ExerciseInputData numberOfSets={sets} setNumberOfSets={setSets}/>
             </div>
-
-
+                {/* Shows button if less than 5 sets */}
+                {/* Shows if the add button has been clicked, so add button doesn't appear with these */}
         </div>
     </div>
   )
